d08b02ea534490f2be133ebe4cdc31bc541728ed
vms <michail.vms@gmail.com>
2019-03-05 23:00:06 +0300

    update json scheme in tic-tac-toe

--- tic-tac-toe/backend/src/lib.rs
+++ tic-tac-toe/backend/src/lib.rs
@@ -20,20 +20,21 @@ mod tests;
 mod error_type;
 mod game;
 mod game_manager;
-mod json_parser;
 mod player;
+mod request_response;
 
 use crate::error_type::AppResult;
 use crate::game_manager::GameManager;
-use crate::json_parser::*;
+use crate::request_response::{Request, Response};
 
 use fluence::sdk::*;
-use serde_json::{json, Value};
+use serde_json::Value;
 use std::cell::RefCell;
 
 mod settings {
     pub const PLAYERS_MAX_COUNT: usize = 1024;
     pub const GAMES_MAX_COUNT: usize = 1024;
+    pub const SEED: u64 = 12345678;
     // to prevent DoS attack with large strings
     pub const USER_NAME_MAX_LEN: usize = 1024;
 }
@@ -43,49 +44,42 @@ thread_local! {
 }
 
 fn do_request(req: String) -> AppResult<Value> {
-    let raw_request: Value = serde_json::from_str(req.as_str())?;
-    let request: Request = serde_json::from_value(raw_request.clone())?;
+    let request: Request = serde_json::from_str(req.as_str())?;
 
-    match request.action.as_str() {
-        "move" => {
-            let player_move: PlayerMove = serde_json::from_value(raw_request)?;
-            GAME_MANAGER.with(|gm| {
-                gm.borrow()
-                    .make_move(request.player_name, player_move.coords)
-            })
+    match request {
+        Request::PlayerMove {
+            player_name,
+            coords,
+        } => GAME_MANAGER.with(|gm| gm.borrow().make_move(player_name, coords)),
+
+        Request::CreatePlayer { player_name } => {
+            GAME_MANAGER.with(|gm| gm.borrow_mut().create_player(player_name))
         }
 
-        "create_player" => {
-            GAME_MANAGER.with(|gm| gm.borrow_mut().create_player(request.player_name))
-        }
-
-        "create_game" => {
-            let player_tile: PlayerTile = serde_json::from_value(raw_request)?;
-            let player_tile = game::Tile::from_char(player_tile.tile).ok_or_else(|| {
+        Request::CreateGame { player_name, player_tile } => {
+            let player_tile = game::Tile::from_char(player_tile).ok_or_else(|| {
                 "incorrect tile type, please choose it from {'X', 'O'} set".to_owned()
             })?;
-
-            GAME_MANAGER.with(|gm| {
-                gm.borrow_mut()
-                    .create_game(request.player_name, player_tile)
-            })
+            GAME_MANAGER.with(|gm| gm.borrow_mut().create_game(player_name, player_tile))
         }
 
-        "get_game_state" => GAME_MANAGER.with(|gm| gm.borrow().get_game_state(request.player_name)),
+        Request::GetGameState { player_name } => {
+            GAME_MANAGER.with(|gm| gm.borrow().get_game_state(player_name))
+        }
 
-        "get_statistics" => GAME_MANAGER.with(|gm| gm.borrow().get_statistics()),
-
-        _ => Err(format!("{} action key is unsupported", request.action)).map_err(Into::into),
+        Request::GetStatistics => GAME_MANAGER.with(|gm| gm.borrow().get_statistics()),
     }
 }
 
 #[invocation_handler]
 fn main(req: String) -> String {
     match do_request(req) {
-        Ok(req) => req.to_string(),
-        Err(err) => json!({
-            "error": err.to_string()
-        })
-        .to_string(),
+        Ok(res) => res.to_string(),
+        Err(err) => {
+            let response = Response::Error {
+                error: err.to_string(),
+            };
+            serde_json::to_string(&response).unwrap()
+        }
     }
 }
