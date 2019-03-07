61f14a1186378df90bce7d557e2c37838c22add1
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-03-07 11:16:32 +0300

    update appIds

--- tic-tac-toe/frontend/index.js
+++ tic-tac-toe/frontend/index.js
@@ -22,7 +22,7 @@ window.onload = function () {
 	let ethUrl = "http://data.fluence.one:8545/";
 
 	// application to interact with that stored in Fluence contract
-	let appId = "13";
+	let appId = "2";
 
 	// create a session between client and backend application
 	fluence.connect(contractAddress, appId, ethUrl).then((s) => {
