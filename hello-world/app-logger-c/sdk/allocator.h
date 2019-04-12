53fddff1d9d374adb6a282a58bdb636221d30c26
vms <michail.vms@gmail.com>
2019-04-12 15:00:46 +0300

    add c hello-world app with logger

---
+++ hello-world/app-logger-c/sdk/allocator.h
@@ -0,0 +1,9 @@
+#ifndef FLUENCE_C_SDK_ALLOCATOR_H
+#define FLUENCE_C_SDK_ALLOCATOR_H
+
+#include <stddef.h> // for size_t
+
+void *allocate(size_t size);
+void deallocate(void *ptr, size_t size);
+
+#endif //FLUENCE_C_SDK_ALLOCATOR_H
---
+++ hello-world/app-logger-c/sdk/logger.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_WORLD_LOGGER_H
+#define HELLO_WORLD_LOGGER_H
+
+void wasm_log(const char *str, int len);
+
+#endif //HELLO_WORLD_LOGGER_H
